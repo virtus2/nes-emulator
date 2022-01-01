2cdf0d302189d56c04376a4b6c60c29be2929905
virtus2 <khd1326@naver.com>
2022-01-02 05:04:35 +0900

    에뮬레이터 테스트를 위한 Demo class 생성

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 ﻿#include "Bus.h"
 #include "nes6502.h"
+#include "Engine.h"
 
-int main()
+class Demo : public Engine
 {
+    Bus nes;
+    std::map<uint16_t, std::string> mapAsm;
+    std::string hex(uint32_t n, uint8_t d)
+    {
+        std::string s(d, '0');
+        for (int i = d - 1; i >= 0; i--, n >>= 4)
+            s[i] = "0123456789ABCDEF"[n & 0xF];
+        return s;
+    }
+};
+
+int main(int argc, char* args[])
+{
+    Demo demo;
+    demo.Init(680, 480);
+    demo.Start();
     return 0;
 }
