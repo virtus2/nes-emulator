7aa048b56332d647959de75ba0976f5c30f63ca7
virtus2 <khd1326@naver.com>
2022-01-16 03:41:43 +0900

    테스트 위한 함수 구현

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
-﻿#include "Bus.h"
+﻿#include <iostream>
+#include <sstream>
+
+#include "Bus.h"
 #include "nes6502.h"
+
+#define VTS_ENGINE
 #include "Engine.h"
 
-class Demo : public Engine
+class Demo : public vts::Engine
 {
 	public:
     Bus nes;
+	std::shared_ptr<Cartridge> cart;
+	bool bEmulationRun = false;
+	float fResidualTime = 0.0f;
+
+	private:
     std::map<uint16_t, std::string> mapAsm;
+	
     std::string hex(uint32_t n, uint8_t d)
 	{
         std::string s(d, '0');
@@ -33,15 +44,15 @@ class Demo : public Engine
 	void DrawCpu(int x, int y)
 	{
 		std::string status = "STATUS: ";
-		DrawString(x, y, "STATUS:", WHITE);
-		DrawString(x + 64, y, "N", nes.cpu.status & nes6502::N ? WHITE : RED);
-		DrawString(x + 80, y, "V", nes.cpu.status & nes6502::V ? GREEN : RED);
-		DrawString(x + 96, y, "-", nes.cpu.status & nes6502::U ? GREEN : RED);
-		DrawString(x + 112, y, "B", nes.cpu.status & nes6502::B ? GREEN : RED);
-		DrawString(x + 128, y, "D", nes.cpu.status & nes6502::D ? GREEN : RED);
-		DrawString(x + 144, y, "I", nes.cpu.status & nes6502::I ? GREEN : RED);
-		DrawString(x + 160, y, "Z", nes.cpu.status & nes6502::Z ? GREEN : RED);
-		DrawString(x + 178, y, "C", nes.cpu.status & nes6502::C ? GREEN : RED);
+		DrawString(x, y, "STATUS:", vts::WHITE);
+		DrawString(x + 64, y, "N", nes.cpu.status & nes6502::N ? vts::WHITE : vts::RED);
+		DrawString(x + 80, y, "V", nes.cpu.status & nes6502::V ? vts::GREEN : vts::RED);
+		DrawString(x + 96, y, "-", nes.cpu.status & nes6502::U ? vts::GREEN : vts::RED);
+		DrawString(x + 112, y, "B", nes.cpu.status & nes6502::B ? vts::GREEN : vts::RED);
+		DrawString(x + 128, y, "D", nes.cpu.status & nes6502::D ? vts::GREEN : vts::RED);
+		DrawString(x + 144, y, "I", nes.cpu.status & nes6502::I ? vts::GREEN : vts::RED);
+		DrawString(x + 160, y, "Z", nes.cpu.status & nes6502::Z ? vts::GREEN : vts::RED);
+		DrawString(x + 178, y, "C", nes.cpu.status & nes6502::C ? vts::GREEN : vts::RED);
 		DrawString(x, y + 10, "PC: $" + hex(nes.cpu.pc, 4));
 		DrawString(x, y + 20, "A: $" + hex(nes.cpu.a, 2) + "  [" + std::to_string(nes.cpu.a) + "]");
 		DrawString(x, y + 30, "X: $" + hex(nes.cpu.x, 2) + "  [" + std::to_string(nes.cpu.x) + "]");
@@ -55,7 +66,7 @@ class Demo : public Engine
 		int nLineY = (nLines >> 1) * 10 + y;
 		if (it_a != mapAsm.end())
 		{
-			DrawString(x, nLineY, (*it_a).second, CYAN);
+			DrawString(x, nLineY, (*it_a).second, vts::CYAN);
 			while (nLineY < (nLines * 10) + y)
 			{
 				nLineY += 10;
@@ -83,82 +94,73 @@ class Demo : public Engine
 
 	bool OnUserCreate()
 	{
-		// Load Program (assembled at https://www.masswerk.at/6502/assembler.html)
-		/*
-			*=$8000
-			LDX #10
-			STX $0000
-			LDX #3
-			STX $0001
-			LDY $0000
-			LDA #0
-			CLC
-			loop
-			ADC $0001
-			DEY
-			BNE loop
-			STA $0002
-			NOP
-			NOP
-			NOP
-		*/
-
-		// Convert hex string into bytes for RAM
-		std::stringstream ss;
-		ss << "A2 0A 8E 00 00 A2 03 8E 01 00 AC 00 00 A9 00 18 6D 01 00 88 D0 FA 8D 02 00 EA EA EA";
-		uint16_t nOffset = 0x8000;
-		while (!ss.eof())
+		// Load the cartridge
+		cart = std::make_shared<Cartridge>("nestest.nes");
+		if (!cart->ImageValid())
 		{
-			std::string b;
-			ss >> b;
-			nes.cpuRam[nOffset++] = (uint8_t)std::stoul(b, nullptr, 16);
+			printf("Cartridge Not Valid!\n");
+			return false;
 		}
 
-		// Set Reset Vector
-		nes.cpuRam[0xFFFC] = 0x00;
-		nes.cpuRam[0xFFFD] = 0x80;
-
-		// Dont forget to set IRQ and NMI vectors if you want to play with those
+		// Insert into NES
+		nes.insertCartridge(cart);
 
 		// Extract dissassembly
 		mapAsm = nes.cpu.disassemble(0x0000, 0xFFFF);
 
-		// Reset
-		nes.cpu.reset();
+		// Reset NES
+		nes.reset();
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime)
 	{
-		Clear(DARK_BLUE);
+		Clear(vts::DARK_BLUE);
 
-
-		if (GetKey(SDLK_SPACE).bPressed)
+		if (bEmulationRun)
 		{
-			do
+			if (fResidualTime > 0.0f)
+				fResidualTime -= fElapsedTime;
+			else
 			{
-				nes.cpu.clock();
-			} while (!nes.cpu.complete());
+				fResidualTime += (1.0f / 60.0f) - fElapsedTime;
+				do { nes.clock(); } while (!nes.ppu.frame_complete);
+				nes.ppu.frame_complete = false;
+			}
+		}
+		else
+		{
+			// Emulate code step-by-step
+			if (GetKey(SDLK_c).bPressed)
+			{
+				// Clock enough times to execute a whole CPU instruction
+				do { nes.clock(); } while (!nes.cpu.complete());
+				// CPU clock runs slower than system clock, so it may be
+				// complete for additional system clock cycles. Drain
+				// those out
+				do { nes.clock(); } while (nes.cpu.complete());
+			}
+
+			// Emulate one whole frame
+			if (GetKey(SDLK_f).bPressed)
+			{
+				// Clock enough times to draw a single frame
+				do { nes.clock(); } while (!nes.ppu.frame_complete);
+				// Use residual clock cycles to complete current instruction
+				do { nes.clock(); } while (!nes.cpu.complete());
+				// Reset frame completion flag
+				nes.ppu.frame_complete = false;
+			}
 		}
 
-		if (GetKey(SDLK_r).bPressed)
-			nes.cpu.reset();
 
-		if (GetKey(SDLK_i).bPressed)
-			nes.cpu.irq();
+		if (GetKey(SDLK_SPACE).bPressed) bEmulationRun = !bEmulationRun;
+		if (GetKey(SDLK_r).bPressed) nes.reset();
 
-		if (GetKey(SDLK_n).bPressed)
-			nes.cpu.nmi();
+		DrawCpu(516, 2);
+		DrawCode(516, 72, 26);
 
-		// Draw Ram Page 0x00		
-		DrawRam(2, 2, 0x0000, 16, 16);
-		DrawRam(2, 182, 0x8000, 16, 16);
-		DrawCpu(448, 2);
-		DrawCode(448, 72, 26);
-
-
-		DrawString(10, 370, "SPACE = Step Instruction    R = RESET    I = IRQ    N = NMI");
-
+		DrawSprite(0, 0, &nes.ppu.GetScreen(), 2);
 		return true;
 	}
 };
@@ -166,7 +168,7 @@ class Demo : public Engine
 int main(int argc, char* args[])
 {
     Demo demo;
-    demo.Init(680, 480);
+    demo.Init(780, 480);
     demo.Start();
     return 0;
 }
